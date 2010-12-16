bf793f080dffc8d19bedaf7237a3fabb9dc1576e
ymqqqqdx <ymqqqqdx@163.com>
2010-12-16 19:42:41 +0800

    none

--- bash/commitmynote.sh
+++ bash/commitmynote.sh
@@ -10,12 +10,15 @@ cd ~/Documents/C_practice/
 rm *.swp
 echo junkfile has been removed!
 echo
+echo adding files.....
 git add .
+echo added
+echo -n Enter commit message:
 read -e MSG
-git commit -a -m "$MSG:-No-Information"
+git commit -a -m "${MSG:-No-Information}"
 echo -n "Wheter push it to remote master(Y/n)"
 read CHOICE
-if [ "$CHOICE:-y" = "y" ]
+if [ "${CHOICE:-y}" = "y" ]
 then
     echo pushing...........
     git push

7bfbb288f5f8032c83110d6d98f0aecf07d35f8a
ymqqqqdx <ymqqqqdx@163.com>
2010-12-16 20:03:59 +0800

    new

--- bash/commitmynote.sh
+++ bash/commitmynote.sh
@@ -7,12 +7,30 @@ then
 fi
 
 cd ~/Documents/C_practice/
+for dir in `ls`
+do
+    if [ -d $dir ]
+    then
+        cd $dir
+        echo Scanning ./$dir
+        rm -v *.swp
+        rm -v *.out
+        cd ..
+    fi
+done
 rm *.swp
 echo junkfile has been removed!
 echo
 echo adding files.....
 git add .
 echo added
+echo -n "Whether commit it:(y/N)"
+read COM
+if [ "${COM:-n}" = "n" ]
+then
+    echo "cancelled!" 1>&2
+    exit 1
+fi
 echo -n Enter commit message:
 read -e MSG
 git commit -a -m "${MSG:-No-Information}"

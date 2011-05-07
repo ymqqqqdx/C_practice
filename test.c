d0864c3041b229431c0fc1651546e409cb62fc96
ymqqqqdx <ymqqqqdx@ubuntu-desktop.(none)>
2011-05-07 20:58:13 +0800

    234

--- test.c
+++ test.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main(int argc, const char *argv[])
 {
-    char buf[20];
-    strcpy(buf, "hello");
-    puts(buf);
-    printf("%d\n", strlen(buf));
-    while(1);
+    int i;
+    char *mall = malloc(10);
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d\n", *(mall + i));
+    }
     return 0;
 }
